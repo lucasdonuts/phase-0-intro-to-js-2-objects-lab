e4317d51fba67f74c6a50eaa45a2e6b214eab2c2
Lucas Duncan <lucasdonuts@gmail.com>
2022-06-28 16:22:09 -0500

    commit tree 99d6e3a96498182d0d0a422830dd6f97438006d3

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+  name: "Lucas",
+  streetAddress: "123 Four Street"
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+  return {
+    ...obj,
+    [key]: value
+  }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+  obj[key] = value;
+
+  return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+  const newObj = {...obj};
+
+  delete newObj[key];
+  return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+  delete obj[key];
+  
+  return obj;
+}
